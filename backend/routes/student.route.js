b43e0c90dc959469cfb2747990a64facbdf8a874
Shubham9049 <shubham.rajveer19@gmail.com>
2026-02-03 12:03:08 +0530

    changes done

--- backend/routes/student.route.js
+++ backend/routes/student.route.js
@@ -11,19 +11,14 @@ const {
 
 const router = express.Router();
 
-// Create student (Admin only)
 router.post("/", authMiddleware, isAdmin, createStudent);
 
-// Get all students (Admin + User)
 router.get("/", authMiddleware, getStudents);
 
-// Get student by ID (Admin + User)
 router.get("/:id", authMiddleware, getStudentById);
 
-// Update student (Admin only)
 router.put("/:id", authMiddleware, isAdmin, updateStudent);
 
-// Delete student (Admin only)
 router.delete("/:id", authMiddleware, isAdmin, deleteStudent);
 
 module.exports = router;
